2e671c4b058905be555f2e7d6ab77a028143f604
like <20783506632@qq.com>
2017-04-07 21:47:20 +0800

    implement part4

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170406141942) do
+ActiveRecord::Schema.define(version: 20170407132149) do
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.integer  "wage_upper_lound"
     t.integer  "wage_lower_lound"
     t.string   "contacct_email"
     t.integer  "wage_upper_bound"
     t.integer  "wage_lower_bound"
     t.string   "contact_email"
+    t.boolean  "is_hidden",        default: true
   end
 
   create_table "users", force: :cascade do |t|
